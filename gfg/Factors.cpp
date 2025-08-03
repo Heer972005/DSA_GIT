b57f8396a865e120771b9ae5b43e8f0d0e848651
Heer972005 <mehtaheer.972@gmail.com>
2025-08-03 20:00:31 +0530

    prime number

--- gfg/Factors.cpp
+++ gfg/Factors.cpp
@@ -9,9 +9,28 @@ vector<int>printDivisors(int n){
     }
     return divisors;
 }
+vector<int>SQprintDivisors(int n){
+    vector<int> divisors;
+    for (int i = 1; i <= sqrt(n);i++){
+        if(n%i==0){
+            if(n/i==i)
+                divisors.push_back(i);
+            else{
+                divisors.push_back(i);
+                divisors.push_back(n / i);
+            }
+        }
+    }
+    return divisors;
+}
 int main(){
     vector<int> divisors = printDivisors(10);
+    vector<int> div = SQprintDivisors(10);
     for(auto &div:divisors){
-        cout << div << "";
+        cout << div << " ";
+    }
+    cout << "\n";
+    for(auto &div:div){
+        cout << div << " ";
     }
 }

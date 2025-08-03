bd648c69f42ca474ecdf95113fd8aee67d71b91e
Heer972005 <mehtaheer.972@gmail.com>
2025-08-03 18:59:11 +0530

    factors of a number

---
+++ gfg/Factors.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+vector<int>printDivisors(int n){
+    vector<int> divisors;
+    for (int i = 1; i <= n;i++){
+        if(n%i==0){
+            divisors.push_back(i);
+        }
+    }
+    return divisors;
+}
+int main(){
+    vector<int> divisors = printDivisors(10);
+    for(auto &div:divisors){
+        cout << div << "";
+    }
+}
